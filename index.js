ea914b6400dfa63ef89198fd2dc25f029fadbd2e
Alex Holhos <alex.holhos@emanuel.ro>
2018-02-17 16:57:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const app = "I do do much."
 function destructivelyAppendKitten(name){
- //var int = [].concat(kittens);
  var int = kittens;
   int.push(name);
+}
+function destructivelyRemoveKitten(){
+ //var int = [].concat(kittens);
+ var int = kittens;
+  int.unshift;
 }
